cb9cf98dd6e030b8a2e92d0a170e810248c36530
name <phr038942@gmail.com>
2022-03-17 23:37:13 +0900

    spring container study 03/17

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,24 +1,69 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// 공연 기획자와 같이, 애플리케이션의 전체 동작 방식을 구성(=설정)하는 클래스
-// 기존에 impl(클라)에서 일일이 new 생성자로 처음 선언했던 구현 클래스 부분을, 아래의 방식으로 통일하여 정리
+/**  03.17 추가 : AppConfig Spring 기반으로 변경하기 By Annotation
+ *   1) 먼저 클래스 앞에다가 Configuration(설정정보, 구성정보를 뜻함) 붙여주기
+ */
+@Configuration
 public class AppConfig {
 
+    /**
+     * AppConfig : 애플리케이션의 전체 동작 방식을 구성(=설정)하는 클래스 like (공연에서의) 공연 기획자.
+     * 기존에 impl(클라이언트)에서 일일이 new 생성자로 처음 선언했던 구현 클래스 부분을, 아래의 방식으로 통일하여 정리한다!
+     */
+
     // 어디선가 AppConfig를 통해 memberService를 불러서 쓰게 되면 (=> new MemoryMemberRepository())
+//    public MemberService memberService(){
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+
+//    public OrderService orderService(){
+//        return new OrderServiceImpl(
+//                new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+
+    // 03.16 추가(AppConfig 리펙토링) : 위 코드로도 DIP를 잘 지킬 수 있지만, 각 역할에 따른 구현을 한눈에 보고 이해할 수는 없다.
+    // 이 역할을 잘 드러나도록 리펙토링이 필요하다!
+    // 결론 : 아래와 같이 변경하면, 각 메소드별 역할을 명확하게 알 수 있다.
+
+    /** AppConfig Spring 기반으로 변경
+     *  2) 각 메소드 앞에 @Bean(Spring Container를 뜻함) 붙여주기
+     */
+    @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        // ==> ctrl + alt + m 단축키(리펙토링 메소드)로 new MemoryMemberRepository()를 변경 + 아래 메소드의 선언부는 interface를 가리키도록
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    // ==> ctrl + alt + m 단축키(리펙토링 메소드)로 new FixDiscountPolicy()를 변경 + 아래 메소드의 선언부는 interface를 가리키도록
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(), new FixDiscountPolicy());
+                new MemoryMemberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        // 03.16 두번째 수정 : 만약 할인 정책에 수정이 들어간다면? (고정 => 비율정책으로...)
+        // 아래와 같이 discountPolicy만 바꾸면 ok
+    //    return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -3,10 +3,10 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
@@ -14,10 +14,20 @@ public class OrderApp {
 //      MemberService memberService = new MemberServiceImpl(null);
 //      OrderService orderService = new OrderServiceImpl(null, null);
 
-        //03.15 의존성주입 잘 된 version
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        /**03.15 의존성주입 잘 된 version
+         AppConfig appConfig = new AppConfig();
+         MemberService memberService = appConfig.memberService();
+         OrderService orderService = appConfig.orderService();
+         */
+
+        /**03.17 (위 코드를) Spring 기반으로 변경
+         1) 우측 선언문의 AppConfig.class를 통해, Spring이 AppConfig에서 @Bean으로 등록된 객체들을 컨테이너에 넣어서 관리할 수 있다.
+         2) 컨테이너에 등록된 각각의 메소드는 이름을 통해 꺼내올 수 있다.(기본적으로 메소드명을 통해 객체가 스프링 컨테이너에 저장된다.)
+         프로그램 실행시, 로그를 통해 Spring 컨테이너에 각각의 객체가 저장되는것 확인 가능하다!
+         */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // 1)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 2)
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);// 2)
 
         // 계정생성
         Long memberId = 1L;
